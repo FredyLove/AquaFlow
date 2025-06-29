62232302d2d17876c1c515984eb6fa3344a6d057
Tagne Fokouong Olivares Alfred <fredylove614@gmail.com>
2025-06-29 02:24:47 +0100

    a

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,14 +1,15 @@
+// src/components/ProtectedRoute.jsx
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = ({ allowedRole }) => {
   const token = localStorage.getItem("token");
-  const user = JSON.parse(localStorage.getItem("user")); // You must store user data after login
+  const user = JSON.parse(localStorage.getItem("user"));
 
   if (!token || !user) {
     return <Navigate to="/customer-login" />;
   }
 
-  if (allowedRole && user.role !== allowedRole) {
+  if (user.role !== allowedRole) {
     return <Navigate to="/customer-portal" />;
   }
 
